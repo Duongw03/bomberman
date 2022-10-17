6fbd1e7841fbc293e0306772779468e91dffac66
Duongw03 <113883649+Duongw03@users.noreply.github.com>
2022-10-17 14:47:04 +0700

    Add files via upload

---
+++ demo3/src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module uet.oop.bomberman {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+    requires java.desktop;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+
+    opens uet.oop.bomberman to javafx.fxml;
+    exports uet.oop.bomberman;
+}
